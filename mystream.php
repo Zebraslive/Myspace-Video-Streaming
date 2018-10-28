8706b1b3b4daada99e57f12314b358bead952b8b
Dinosaur Burger <28460277+Zebraslive@users.noreply.github.com>
2018-10-27 21:27:27 -0400

    Important changes
    added CURLOPT_SSL_VERIFYPEER and curl function.

--- mystream.php
+++ mystream.php
@@ -1,6 +1,17 @@
 <?php
+function curlit($url){
+       $ch = curl_init();
+$timeout = 8;
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //don't change!
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$htmlf = curl_exec($ch);
+curl_close($ch);
+return $htmlf;
+}
 function myStream($myspacelink) {
-$htmlf = file_get_contents($myspacelink);
+$htmlf = curlit($myspacelink);
 $fsk = "";
 $internalErrors = libxml_use_internal_errors(true);
 $dom = new DOMDocument();
@@ -10,15 +21,14 @@ if ($k >= 5) {
        $fsk = $js->nodeValue;
      break;
     }
-
 }
 $parts = explode('mp4StreamUrl":"', $fsk);
 $partsx = explode('","isPremium"', $parts[1]);
 $myvidx = $partsx[0]; //Stream mp4 url
-return $myvidx;
+return $myvidx;   
 }
 function grabImg($url) {
-$htmlf = file_get_contents($url);
+$htmlf = curlit($url);
 $fsk = "";
 $internalErrors = libxml_use_internal_errors(true);
 $dom = new DOMDocument();
@@ -28,11 +38,10 @@ if ($k >= 5) {
        $fsk = $js->nodeValue;
      break;
     }
-
 }
 $parts = explode('"Still":"', $fsk);
 $partsx = explode('"},"uid":"', $parts[1]);
 $posterimg = $partsx[0]; //poster img
-return $posterimg;
+return $posterimg;  
 }
 ?>
